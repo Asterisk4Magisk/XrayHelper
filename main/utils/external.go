ef32623b53419f4974a90ace0ca4533c21ecc80d
whalechoi <cj1369636717@gmail.com>
2023-05-11 19:13:03 +0800

    init2

--- main/utils/external.go
+++ main/utils/external.go
@@ -2,42 +2,99 @@ package utils
 
 import (
 	"bytes"
+	"context"
+	"errors"
 	"os/exec"
+	"time"
 )
 
 type External interface {
 	Err() error
-	Stdout() string
-	Stderr() string
+	Cmd() *exec.Cmd
+	Stdout() *bytes.Buffer
+	Stderr() *bytes.Buffer
 	Run()
+	RunWithTimeout(timeout time.Duration)
+	Start()
+	StartWithTimeout(timeout time.Duration)
 }
 
 type external struct {
+	name   string
+	arg    []string
 	cmd    *exec.Cmd
-	stdout string
-	stderr string
+	stdout *bytes.Buffer
+	stderr *bytes.Buffer
 	err    error
 }
 
-func NewExternal(cmd *exec.Cmd) External {
-	return &external{cmd: cmd}
+func NewExternal(name string, arg ...string) External {
+	return &external{name: name, arg: arg}
 }
 
 func (this *external) Run() {
+	this.cmd = exec.Command(this.name, this.arg...)
 	var stdout bytes.Buffer
 	var stderr bytes.Buffer
+	this.stdout = &stdout
+	this.stderr = &stderr
 	this.cmd.Stdout = &stdout
 	this.cmd.Stderr = &stderr
 	this.err = this.cmd.Run()
-	this.stdout = stdout.String()
-	this.stderr = stderr.String()
 }
 
-func (this *external) Stdout() string {
+func (this *external) RunWithTimeout(timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	this.cmd = exec.CommandContext(ctx, this.name, this.arg...)
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	this.stdout = &stdout
+	this.stderr = &stderr
+	this.cmd.Stdout = &stdout
+	this.cmd.Stderr = &stderr
+	this.err = this.cmd.Run()
+	if ctx.Err() == context.DeadlineExceeded {
+		this.err = errors.New("command timed out")
+	}
+}
+
+func (this *external) Start() {
+	this.cmd = exec.Command(this.name, this.arg...)
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	this.stdout = &stdout
+	this.stderr = &stderr
+	this.cmd.Stdout = &stdout
+	this.cmd.Stderr = &stderr
+	this.err = this.cmd.Start()
+}
+
+func (this *external) StartWithTimeout(timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	this.cmd = exec.CommandContext(ctx, this.name, this.arg...)
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+	this.stdout = &stdout
+	this.stderr = &stderr
+	this.cmd.Stdout = &stdout
+	this.cmd.Stderr = &stderr
+	this.err = this.cmd.Start()
+	if ctx.Err() == context.DeadlineExceeded {
+		this.err = errors.New("command timed out")
+	}
+}
+
+func (this *external) Cmd() *exec.Cmd {
+	return this.cmd
+}
+
+func (this *external) Stdout() *bytes.Buffer {
 	return this.stdout
 }
 
-func (this *external) Stderr() string {
+func (this *external) Stderr() *bytes.Buffer {
 	return this.stderr
 }
 
