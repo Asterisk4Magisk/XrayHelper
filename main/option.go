5e06d0f8d95e4137a546d178cc4718029ed11c3f
whalechoi <cj1369636717@gmail.com>
2023-05-11 11:36:43 +0800

    init2

--- main/option.go
+++ main/option.go
@@ -1,43 +1,43 @@
 package main
 
 import (
-    "XrayHelper/main/builds"
-    "XrayHelper/main/commands"
-    "XrayHelper/main/utils"
-    "fmt"
-    "github.com/jessevdk/go-flags"
+	"XrayHelper/main/builds"
+	"XrayHelper/main/commands"
+	"XrayHelper/main/utils"
+	"fmt"
+	"github.com/jessevdk/go-flags"
 )
 
 var Option struct {
-    ConfigFilePath string                  `short:"c" long:"config" default:"./config.yml" description:"specify configuration file"`
-    VerboseFlag    bool                    `long:"verbose" description:"show verbose debug information"`
-    VersionFlag    bool                    `short:"v" long:"version" description:"show current version"`
-    Service        commands.ServiceCommand `command:"service" description:"control xray service"`
-    Proxy          commands.ProxyCommand   `command:"proxy" description:"control system proxy"`
+	ConfigFilePath string                  `short:"c" long:"config" default:"./config.yml" description:"specify configuration file"`
+	VerboseFlag    bool                    `long:"verbose" description:"show verbose debug information"`
+	VersionFlag    bool                    `short:"v" long:"version" description:"show current version"`
+	Service        commands.ServiceCommand `command:"service" description:"control xray service"`
+	Proxy          commands.ProxyCommand   `command:"proxy" description:"control system proxy"`
 }
 
 func LoadOption() {
-    utils.Verbose = &Option.VerboseFlag
-    builds.ConfigFilePath = &Option.ConfigFilePath
-    parser := flags.NewParser(&Option, flags.HelpFlag|flags.PassDoubleDash)
-    _, err := parser.Parse()
-    if err != nil {
-        if _, ok := err.(*flags.Error); ok {
-            typ := err.(*flags.Error).Type
-            if typ == flags.ErrCommandRequired {
-                if Option.VersionFlag {
-                    fmt.Println(builds.Version())
-                }
-                err = nil
-            }
-            if typ == flags.ErrHelp {
-                fmt.Println(builds.VersionStatement())
-                fmt.Println(err.Error())
-                err = nil
-            }
-            utils.HandleError(err)
-        } else {
-            utils.HandleError(err)
-        }
-    }
+	utils.Verbose = &Option.VerboseFlag
+	builds.ConfigFilePath = &Option.ConfigFilePath
+	parser := flags.NewParser(&Option, flags.HelpFlag|flags.PassDoubleDash)
+	_, err := parser.Parse()
+	if err != nil {
+		if _, ok := err.(*flags.Error); ok {
+			typ := err.(*flags.Error).Type
+			if typ == flags.ErrCommandRequired {
+				if Option.VersionFlag {
+					fmt.Println(builds.Version())
+				}
+				err = nil
+			}
+			if typ == flags.ErrHelp {
+				fmt.Println(builds.VersionStatement())
+				fmt.Println(err.Error())
+				err = nil
+			}
+			utils.HandleError(err)
+		} else {
+			utils.HandleError(err)
+		}
+	}
 }
