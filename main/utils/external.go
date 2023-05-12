8cdee04c5366ba04cea171d3d5d411e14127b96c
whalechoi <cj1369636717@gmail.com>
2023-05-12 10:50:48 +0800

    init3

--- main/utils/external.go
+++ main/utils/external.go
@@ -1,9 +1,10 @@
 package utils
 
 import (
-	"bytes"
 	"context"
 	"errors"
+	"io"
+	"os"
 	"os/exec"
 	"time"
 )
@@ -11,8 +12,6 @@ import (
 type External interface {
 	Err() error
 	Cmd() *exec.Cmd
-	Stdout() *bytes.Buffer
-	Stderr() *bytes.Buffer
 	Run()
 	RunWithTimeout(timeout time.Duration)
 	Start()
@@ -23,36 +22,40 @@ type external struct {
 	name   string
 	arg    []string
 	cmd    *exec.Cmd
-	stdout *bytes.Buffer
-	stderr *bytes.Buffer
+	stdout io.Writer
+	stderr io.Writer
 	err    error
 }
 
-func NewExternal(name string, arg ...string) External {
-	return &external{name: name, arg: arg}
+func NewExternal(stdout io.Writer, stderr io.Writer, name string, arg ...string) External {
+	return &external{name: name, arg: arg, stdout: stdout, stderr: stderr}
 }
 
 func (this *external) Run() {
+	if this.stdout == nil {
+		this.stdout = os.Stdout
+	}
+	if this.stderr == nil {
+		this.stderr = os.Stderr
+	}
 	this.cmd = exec.Command(this.name, this.arg...)
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	this.stdout = &stdout
-	this.stderr = &stderr
-	this.cmd.Stdout = &stdout
-	this.cmd.Stderr = &stderr
+	this.cmd.Stdout = this.stdout
+	this.cmd.Stderr = this.stderr
 	this.err = this.cmd.Run()
 }
 
 func (this *external) RunWithTimeout(timeout time.Duration) {
+	if this.stdout == nil {
+		this.stdout = os.Stdout
+	}
+	if this.stderr == nil {
+		this.stderr = os.Stderr
+	}
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	this.cmd = exec.CommandContext(ctx, this.name, this.arg...)
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	this.stdout = &stdout
-	this.stderr = &stderr
-	this.cmd.Stdout = &stdout
-	this.cmd.Stderr = &stderr
+	this.cmd.Stdout = this.stdout
+	this.cmd.Stderr = this.stderr
 	this.err = this.cmd.Run()
 	if ctx.Err() == context.DeadlineExceeded {
 		this.err = errors.New("command timed out")
@@ -60,26 +63,31 @@ func (this *external) RunWithTimeout(timeout time.Duration) {
 }
 
 func (this *external) Start() {
+	if this.stdout == nil {
+		this.stdout = os.Stdout
+	}
+	if this.stderr == nil {
+		this.stderr = os.Stderr
+	}
 	this.cmd = exec.Command(this.name, this.arg...)
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	this.stdout = &stdout
-	this.stderr = &stderr
-	this.cmd.Stdout = &stdout
-	this.cmd.Stderr = &stderr
+
+	this.cmd.Stdout = this.stdout
+	this.cmd.Stderr = this.stderr
 	this.err = this.cmd.Start()
 }
 
 func (this *external) StartWithTimeout(timeout time.Duration) {
+	if this.stdout == nil {
+		this.stdout = os.Stdout
+	}
+	if this.stderr == nil {
+		this.stderr = os.Stderr
+	}
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 	this.cmd = exec.CommandContext(ctx, this.name, this.arg...)
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
-	this.stdout = &stdout
-	this.stderr = &stderr
-	this.cmd.Stdout = &stdout
-	this.cmd.Stderr = &stderr
+	this.cmd.Stdout = this.stdout
+	this.cmd.Stderr = this.stderr
 	this.err = this.cmd.Start()
 	if ctx.Err() == context.DeadlineExceeded {
 		this.err = errors.New("command timed out")
@@ -88,14 +96,6 @@ func (this *external) StartWithTimeout(timeout time.Duration) {
 
 func (this *external) Cmd() *exec.Cmd {
 	return this.cmd
-}
-
-func (this *external) Stdout() *bytes.Buffer {
-	return this.stdout
-}
-
-func (this *external) Stderr() *bytes.Buffer {
-	return this.stderr
 }
 
 func (this *external) Err() error {
