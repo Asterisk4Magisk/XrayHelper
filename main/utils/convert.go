eff927412bce51db17e17da1b130f7444d6f8edb
whalechoi <cj1369636717@gmail.com>
2023-05-22 11:31:25 +0800

    use base64.StdEncoding

--- main/utils/convert.go
+++ main/utils/convert.go
@@ -7,7 +7,7 @@ import (
 
 // DecodeBase64 decode base64 data
 func DecodeBase64(data string) (string, error) {
-	decode, err := base64.URLEncoding.DecodeString(data)
+	decode, err := base64.StdEncoding.DecodeString(data)
 	if err != nil {
 		return "", errors.New("decode base64 data failed, ", err).WithPrefix("convert")
 	}
