2e8ca12281def4c0aa32198bf6e7546e4df9554b
whalechoi <cj1369636717@gmail.com>
2023-06-07 11:30:18 +0800

    base64,when StdEncoding decode failed, try RawStdEncoding

--- main/common/convert.go
+++ main/common/convert.go
@@ -2,14 +2,19 @@ package common
 
 import (
 	"XrayHelper/main/errors"
+	"XrayHelper/main/log"
 	"encoding/base64"
 )
 
 // DecodeBase64 decode base64 data
 func DecodeBase64(data string) (string, error) {
-	decode, err := base64.RawStdEncoding.DecodeString(data)
+	decode, err := base64.StdEncoding.DecodeString(data)
 	if err != nil {
-		return "", errors.New("decode base64 data failed, ", err).WithPrefix("convert")
+		log.HandleDebug("use StdEncoding decode base64 failed, " + err.Error())
+		decode, err = base64.RawStdEncoding.DecodeString(data)
+		if err != nil {
+			return "", errors.New("decode base64 data failed, ", err).WithPrefix("convert")
+		}
 	}
 	return string(decode), nil
 }
