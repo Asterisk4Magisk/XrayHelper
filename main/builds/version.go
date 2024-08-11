21a740f4c612b98f3e64bd1ed610aa7e5e151c24
Whale Choi <cj1369636717@gmail.com>
2024-08-11 20:26:40 +0800

    bump version

--- main/builds/version.go
+++ main/builds/version.go
@@ -10,7 +10,7 @@ const (
 	VersionX byte = 1
 	VersionY byte = 4
 	VersionZ byte = 5
-	Build         = "-debug"
+	Build         = "-release"
 	Intro         = "A unified helper for Android to control system proxy.\n\nTelegram channel: https://t.me/Asterisk4Magisk\nTelegram chat: https://t.me/AsteriskFactory\n\nReport issues at https://github.com/Asterisk4Magisk/XrayHelper/issues\n"
 )
 
